f3fed5b3031e769bb48e2bd0684a7617a2441524
Roberts Tūbelis <roberts.tubelis@hwswservices.com>
2026-05-29 09:31:24 +0300

    initial commit

---
+++ src/main/java/com/ugh/ugh/UghApplication.java
@@ -0,0 +1,13 @@
+package com.ugh.ugh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ugh/ugh/UghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugh.ugh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
